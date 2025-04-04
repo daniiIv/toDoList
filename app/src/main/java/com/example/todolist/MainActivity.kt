4a7952f5c2ce75ad7d761a09676b0fa89ec7c229
I751696 <yordanka.ivanova@sap.com>
2025-04-04 23:52:39 +0300

    project

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.todolist
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Surface
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.ui.theme.ToDoListTheme
+import androidx.compose.material3.MaterialTheme
+import np.com.bimalkafle.todoapp.TodoListPage
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        enableEdgeToEdge()
+        setContent {
+            ToDoListTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TodoListPage(todoViewModel)
+                }
+            }
+        }
+    }
+}
+
